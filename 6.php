1d7e693f10a8b18147972ecde5f84bebdebf44b1
rahul-a-bangera <rahul.bangera.999@gmail.com>
2021-01-24 19:16:16 +0530

    questions added

--- 6.php
+++ 6.php
@@ -1,3 +1,6 @@
+<!-- Write a PHP program to keep track of the number of
+visitors visiting the web page and to display this count
+of visitors, with proper headings -->
 <html>
 <head>
 <title>Visitors Count</title>
